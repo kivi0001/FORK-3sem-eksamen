678986577203f431d2a7856cb43dedf5452557b4
Ida <idla0004@stud.kea.dk>
2026-05-05 15:34:50 +0200

    Opstart på button styling.

---
+++ src/app/components/buttons/PrimaryButton.jsx
@@ -0,0 +1,9 @@
+const PrimaryButton = () => {
+  return (
+    <button className="group relative w-48 cursor-pointer overflow-hidden border-y-2 border-button px-10 py-4 font-bold text-button transition-all duration-300 border:ease-in hover:border-y-button-hover hover:text-button-hover ">
+      Indhold her
+    </button>
+  );
+};
+
+export default PrimaryButton;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import PrimaryButton from "./components/buttons/PrimaryButton";
 
 export default function Home() {
   return (
     <div className="">
       <main className="">
         Hej Kia, vi tester nightclub
+        <div>
+          {" "}
+          <PrimaryButton />
+        </div>
       </main>
     </div>
   );
