5901f88bac942a258d5d9fbbfe32db0266f5965a
Ida <idla0004@stud.kea.dk>
2026-05-05 13:04:16 +0200

    Font tilføjet

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Ubuntu } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const UbuntuSans = Ubuntu({
+  variable: "--font-ubuntu-sans",
+  weight: ["400", "500", "700"],
   subsets: ["latin"],
 });
 
@@ -20,9 +16,11 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${UbuntuSans.variable}  h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+      </body>
     </html>
   );
 }
