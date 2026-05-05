d724a8c2aec48b423bb8f75cfd47dd432100e85a
Ida <idla0004@stud.kea.dk>
2026-05-05 14:03:57 +0200

    Footer html opsat: - Billeder og ikoner importeret - Div og P tags lavet. - Mangler: CSS styling.

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -6,7 +9,7 @@ const Footer = () => {
       <section>
         <div>
           <Image
-            src="/assets/Logo.png"
+            src="/assets/icon/Logo_main.svg"
             width={230}
             height={50}
             alt="Logo"
@@ -27,33 +30,66 @@ const Footer = () => {
           </div>
         </div>
         <div>
-          <h3>News</h3>
-          <Image
-            src="/assets/content-img/recent_post1.jpg"
-            width={50}
-            height={50}
-            alt="DJ wearing cap playing music"
-          />
-          <p>
-            Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Laboriosam,
-            aspernatur quis!
-          </p>
-          <p>April 17, 2026</p>
+          <div>
+            <h3>News</h3>
+            <Image
+              src="/assets/content-img/recent_post1.jpg"
+              width={50}
+              height={50}
+              alt="DJ wearing cap playing music"
+            />
+            <p>
+              Lorem ipsum dolor sit amet
+              consectetur adipisicing elit.
+              Laboriosam, aspernatur quis!
+            </p>
+            <p>April 17, 2026</p>
+          </div>
+          <div>
+            <Image
+              src="/assets/content-img/recent_post2.jpg"
+              width={50}
+              height={50}
+              alt="DJ wearing cap playing music"
+            />
+            <p>
+              Lorem ipsum dolor sit amet
+              consectetur adipisicing elit.
+              Laboriosam, aspernatur quis!
+            </p>
+            <p>April 17, 2026</p>
+          </div>
         </div>
         <div>
-          <Image
-            src="/assets/content-img/recent_post2.jpg"
-            width={50}
-            height={50}
-            alt="DJ wearing cap playing music"
-          />
-          <p>
-            Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Laboriosam,
-            aspernatur quis!
-          </p>
-          <p>April 17, 2026</p>
+          <div>
+            <h3>Recent Posts</h3>
+            <FaXTwitter />
+            <p>
+              It is a long established fact that a
+              reader will be distracted by the
+              readable...
+            </p>
+            <p>5 hours ago</p>
+          </div>
+          <div>
+            <FaXTwitter />
+            <p>
+              It is a long established fact that a
+              reader will be distracted by the
+              readable...
+            </p>
+            <p>5 hours ago</p>
+          </div>
+        </div>
+        <div>
+          <p>Night Club - All Rights Reserved</p>
+          <div>
+            <p>Stay Connected With Us</p>
+            <FaFacebookF />
+            <FaXTwitter />
+            <FaInstagram />
+          </div>
+          <p>Copyright © NightClub</p>
         </div>
       </section>
     </footer>
