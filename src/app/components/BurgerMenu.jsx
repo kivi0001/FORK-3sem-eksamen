27253e722233259ef96d435d197da840db709163
Kia <kivi0001@stud.kea.dk>
2026-05-07 13:59:15 +0200

    forsøg på at fikse styling på åbne/lukke knap for popover

--- src/app/components/BurgerMenu.jsx
+++ src/app/components/BurgerMenu.jsx
@@ -2,6 +2,7 @@
 import "@/app/custom.css";
 import { Popover } from "radix-ui";
 import HeaderButton from "./HeaderButton";
+import { RxCross2 } from "react-icons/rx";
 
 const BurgerMenu = () => {
   return (
@@ -13,18 +14,24 @@ const BurgerMenu = () => {
             <span className="popover-trigger"></span>
             <span className="popover-trigger"></span>
           </div>
+          <div className="close-div"></div>
         </Popover.Trigger>
         <Popover.Portal>
-          <Popover.Content className="flex flex-col gap-5 w-screen h-screen items-center justify-center">
-            <HeaderButton>home</HeaderButton>
-            <HeaderButton>events</HeaderButton>
-            <HeaderButton>
-              book table
-            </HeaderButton>
-            <HeaderButton>
-              contact us
-            </HeaderButton>
-            <Popover.Arrow />
+          <Popover.Content className="PopoverContent">
+            <Popover.Close
+              className="absolute top-0 right-5"
+              aria-label="Close"
+            ></Popover.Close>
+            <div className="flex flex-col gap-10 items-center justify-center w-screen h-screen">
+              <HeaderButton>home</HeaderButton>
+              <HeaderButton>events</HeaderButton>
+              <HeaderButton>
+                book table
+              </HeaderButton>
+              <HeaderButton>
+                contact us
+              </HeaderButton>
+            </div>
           </Popover.Content>
         </Popover.Portal>
       </Popover.Root>
