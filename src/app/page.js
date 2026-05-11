76c56fb1b6191b0e000320add874bc23a4df9741
Ida <idla0004@stud.kea.dk>
2026-05-11 15:02:52 +0200

    Opstartet Gallery section til homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import Hero from "./components/homepage/Hero";
 import WelcomeSection from "./components/homepage/welcomesection/WelcomeSection";
+import GallerySection from "./components/homepage/night_club_gallery/GallerySection";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <Hero />
       </Suspense>
       <WelcomeSection />
+      <GallerySection />
     </main>
   );
 }
