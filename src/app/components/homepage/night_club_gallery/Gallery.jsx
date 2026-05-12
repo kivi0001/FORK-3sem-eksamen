0b675df0cece18641dd6eff3a59dd8e62b84ca09
Ida <idla0004@stud.kea.dk>
2026-05-12 12:45:23 +0200

    Pink hover border på gallery billedet lavet.

--- src/app/components/homepage/night_club_gallery/Gallery.jsx
+++ src/app/components/homepage/night_club_gallery/Gallery.jsx
@@ -7,8 +7,12 @@ const Gallery = ({
 }) => {
   return (
     <>
-      <div className="gallery-wrapper">
+      <div className="gallery-wrapper gallery-hover relative">
         <div className="gallery-container">
+          <div className="flex flex-col gallery-overlay border-y-(--pink) border-y-2">
+            <div className="border-s-(--pink) border-bs-transparent border-e-transparent border-be-transparent border-s-40 border-bs-0 border-e-0 border-be-40"></div>
+            <div className="mt-auto border-s-transparent border-bs-transparent border-e-(--pink) border-be-transparent border-s-0 border-bs-40 border-e-40 border-be-0"></div>
+          </div>
           <img src={imagesrc} alt={descprition} />
         </div>
         <div className="hidden">
