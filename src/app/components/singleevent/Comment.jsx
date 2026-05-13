5964cff48cc75ed39e7710a91de0567fac8ebf6e
Kia <kivi0001@stud.kea.dk>
2026-05-13 20:35:19 +0200

    - dato på comments formateret

--- src/app/components/singleevent/Comment.jsx
+++ src/app/components/singleevent/Comment.jsx
@@ -1,12 +1,23 @@
 const Comment = ({ name, content, date }) => {
+  const newDate = new Date(date);
+  let options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const actualDate = new Intl.DateTimeFormat(
+    "en-UK",
+    options,
+  ).format(newDate);
+
   return (
     <div className="flex flex-col gap-2 my-6 max-w-[80ch]">
-      <div className="flex gap-1">
+      <div className="flex gap-2">
         <h3 className="font-bold text-(length:--font-h3)">
           {name} -
         </h3>
         <h3 className="text-(length:--font-p) text-(--pink)">
-          {date}
+          Posted {actualDate}
         </h3>
       </div>
       <p>{content}</p>
