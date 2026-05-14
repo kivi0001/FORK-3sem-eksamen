61a39722967db13bcb5834bd99e42120fa270044
Kia <kivi0001@stud.kea.dk>
2026-05-14 11:21:11 +0200

    - arbejder på overlay for hover og active state for artist/track

--- src/app/components/homepage/club_track_section/Artists.jsx
+++ src/app/components/homepage/club_track_section/Artists.jsx
@@ -2,28 +2,30 @@ import Image from "next/image";
 
 const Artists = () => {
   return (
-    <ul className="flex">
-      <li>
-        <div>
+    <ul className="artists-scroller">
+      <li className="artist-image-hover relative">
+        <button className="artist-track">
           <Image
+            className="w-50 h-50 object-cover"
             src="/assets/content-img/track1.jpg"
             alt="image of artist"
             width={200}
             height={200}
           />
+        </button>
+        <div className="track-hover-overlay flex flex-col">
+          <div className="border-s-(--pink) border-bs-transparent border-e-transparent border-be-transparent border-s-40 border-bs-0 border-e-0 border-be-40"></div>
+          <div className="mt-auto border-s-transparent border-bs-transparent border-e-(--pink) border-be-transparent border-s-0 border-bs-40 border-e-40 border-be-0"></div>
         </div>
-        <div className="track-hover-overlay"></div>
-        <div className="border-s-(--pink) border-bs-transparent border-e-transparent border-be-transparent border-s-40 border-bs-0 border-e-0 border-be-40"></div>
-        <div className="mt-auto border-s-transparent border-bs-transparent border-e-(--pink) border-be-transparent border-s-0 border-bs-40 border-e-40 border-be-0"></div>
         <div className="track-active-overlay">
           <div className="border-s-(--pink) border-bs-transparent border-e-transparent border-be-transparent border-s-40 border-bs-0 border-e-0 border-be-40"></div>
           <div className="flex flex-col items-center mt-auto">
-            <button className="">
+            <button className="play-button cursor-pointer">
               <Image
                 src="/assets/icon/Play_btn.svg"
                 alt="a play button"
-                width={25}
-                height={25}
+                width={35}
+                height={35}
               />
             </button>
           </div>
