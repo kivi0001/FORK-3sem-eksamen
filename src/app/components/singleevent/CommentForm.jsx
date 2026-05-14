72ef09edf21ca072d6bed30343d74ef028c54e1f
Kia <kivi0001@stud.kea.dk>
2026-05-14 15:13:43 +0200

    - zod installeret - comment form opdateret med zod - forsøg på usestate til at lave besked på succesfuld submit

--- src/app/components/singleevent/CommentForm.jsx
+++ src/app/components/singleevent/CommentForm.jsx
@@ -1,5 +1,69 @@
+"use client";
+import { useForm } from "react-hook-form";
+import PrimaryButtons from "../buttons/PrimaryButtons";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+
+const valideringsSkema = z.object({
+  name: z.string().min(2),
+});
+
 const CommentForm = () => {
-  return <section></section>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(valideringsSkema),
+  });
+
+  const [message, setMessage] = useState("");
+  const onSubmit = (data) => {
+    console.log(data);
+    setMessage("Tak!");
+  };
+  return (
+    <section>
+      <h2>leave a comment</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input
+            {...register("name")}
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Your name"
+          ></input>
+          {errors.name && (
+            <div>{errors.name.message}</div>
+          )}
+          <input
+            {...register("email", {
+              required: true,
+            })}
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Your email"
+          ></input>
+        </div>
+        <textarea
+          {...register("comment", {
+            required: true,
+          })}
+          type="text"
+          name="comment"
+          id="comment"
+          placeholder="Your comment"
+        ></textarea>
+        <PrimaryButtons
+          type="submit"
+          textInput="submit"
+        ></PrimaryButtons>
+      </form>
+    </section>
+  );
 };
 
 export default CommentForm;
