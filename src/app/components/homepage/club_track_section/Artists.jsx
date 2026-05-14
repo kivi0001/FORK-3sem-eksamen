1b5ba8892bffa6fcfd80a9710a9b8aee8add9d3f
Kia <kivi0001@stud.kea.dk>
2026-05-14 10:54:02 +0200

    - importeret track sektion til homepage - Artists komponent opbygget med HTML - Tracksektion har fået importeret headline og begge komponenter

--- src/app/components/homepage/club_track_section/Artists.jsx
+++ src/app/components/homepage/club_track_section/Artists.jsx
@@ -1,5 +1,144 @@
+import Image from "next/image";
+
 const Artists = () => {
-  return <div></div>;
+  return (
+    <ul className="flex">
+      <li>
+        <div>
+          <Image
+            src="/assets/content-img/track1.jpg"
+            alt="image of artist"
+            width={200}
+            height={200}
+          />
+        </div>
+        <div className="track-hover-overlay"></div>
+        <div className="border-s-(--pink) border-bs-transparent border-e-transparent border-be-transparent border-s-40 border-bs-0 border-e-0 border-be-40"></div>
+        <div className="mt-auto border-s-transparent border-bs-transparent border-e-(--pink) border-be-transparent border-s-0 border-bs-40 border-e-40 border-be-0"></div>
+        <div className="track-active-overlay">
+          <div className="border-s-(--pink) border-bs-transparent border-e-transparent border-be-transparent border-s-40 border-bs-0 border-e-0 border-be-40"></div>
+          <div className="flex flex-col items-center mt-auto">
+            <button className="">
+              <Image
+                src="/assets/icon/Play_btn.svg"
+                alt="a play button"
+                width={25}
+                height={25}
+              />
+            </button>
+          </div>
+          <p className="text-center mt-5 uppercase">
+            Black Box Funky
+          </p>
+          <div className="mt-auto border-s-transparent border-bs-transparent border-e-(--pink) border-be-transparent border-s-0 border-bs-40 border-e-40 border-be-0"></div>
+        </div>
+      </li>
+      <li>
+        <div>
+          <Image
+            src="/assets/content-img/track2.jpg"
+            alt="image of artist"
+            width={200}
+            height={200}
+          />
+        </div>
+        <div className="track-hover-overlay"></div>
+        <div className="border-s-(--pink) border-bs-transparent border-e-transparent border-be-transparent border-s-40 border-bs-0 border-e-0 border-be-40"></div>
+        <div className="mt-auto border-s-transparent border-bs-transparent border-e-(--pink) border-be-transparent border-s-0 border-bs-40 border-e-40 border-be-0"></div>
+        <div className="track-active-overlay">
+          <div className="border-s-(--pink) border-bs-transparent border-e-transparent border-be-transparent border-s-40 border-bs-0 border-e-0 border-be-40"></div>
+          <button>
+            <Image
+              src="/assets/icon/Play_btn.svg"
+              alt="a play button"
+              width={25}
+              height={25}
+            />
+          </button>
+          <p>Euphoria</p>
+          <div className="mt-auto border-s-transparent border-bs-transparent border-e-(--pink) border-be-transparent border-s-0 border-bs-40 border-e-40 border-be-0"></div>
+        </div>
+      </li>
+      <li>
+        <div>
+          <Image
+            src="/assets/content-img/track3.jpg"
+            alt="image of artist"
+            width={200}
+            height={200}
+          />
+        </div>
+        <div className="track-hover-overlay"></div>
+        <div className="border-s-(--pink) border-bs-transparent border-e-transparent border-be-transparent border-s-40 border-bs-0 border-e-0 border-be-40"></div>
+        <div className="mt-auto border-s-transparent border-bs-transparent border-e-(--pink) border-be-transparent border-s-0 border-bs-40 border-e-40 border-be-0"></div>
+        <div className="track-active-overlay">
+          <div className="border-s-(--pink) border-bs-transparent border-e-transparent border-be-transparent border-s-40 border-bs-0 border-e-0 border-be-40"></div>
+          <button>
+            <Image
+              src="/assets/icon/Play_btn.svg"
+              alt="a play button"
+              width={25}
+              height={25}
+            />
+          </button>
+          <p>Fashion Red Tape</p>
+          <div className="mt-auto border-s-transparent border-bs-transparent border-e-(--pink) border-be-transparent border-s-0 border-bs-40 border-e-40 border-be-0"></div>
+        </div>
+      </li>
+      <li>
+        <div>
+          <Image
+            src="/assets/content-img/track4.jpg"
+            alt="image of artist"
+            width={200}
+            height={200}
+          />
+        </div>
+        <div className="track-hover-overlay"></div>
+        <div className="border-s-(--pink) border-bs-transparent border-e-transparent border-be-transparent border-s-40 border-bs-0 border-e-0 border-be-40"></div>
+        <div className="mt-auto border-s-transparent border-bs-transparent border-e-(--pink) border-be-transparent border-s-0 border-bs-40 border-e-40 border-be-0"></div>
+        <div className="track-active-overlay">
+          <div className="border-s-(--pink) border-bs-transparent border-e-transparent border-be-transparent border-s-40 border-bs-0 border-e-0 border-be-40"></div>
+          <button>
+            <Image
+              src="/assets/icon/Play_btn.svg"
+              alt="a play button"
+              width={25}
+              height={25}
+            />
+          </button>
+          <p>Neon Nanny</p>
+          <div className="mt-auto border-s-transparent border-bs-transparent border-e-(--pink) border-be-transparent border-s-0 border-bs-40 border-e-40 border-be-0"></div>
+        </div>
+      </li>
+      <li>
+        <div>
+          <Image
+            src="/assets/content-img/track5.jpg"
+            alt="image of artist"
+            width={200}
+            height={200}
+          />
+        </div>
+        <div className="track-hover-overlay"></div>
+        <div className="border-s-(--pink) border-bs-transparent border-e-transparent border-be-transparent border-s-40 border-bs-0 border-e-0 border-be-40"></div>
+        <div className="mt-auto border-s-transparent border-bs-transparent border-e-(--pink) border-be-transparent border-s-0 border-bs-40 border-e-40 border-be-0"></div>
+        <div className="track-active-overlay">
+          <div className="border-s-(--pink) border-bs-transparent border-e-transparent border-be-transparent border-s-40 border-bs-0 border-e-0 border-be-40"></div>
+          <button>
+            <Image
+              src="/assets/icon/Play_btn.svg"
+              alt="a play button"
+              width={25}
+              height={25}
+            />
+          </button>
+          <p>Overload</p>
+          <div className="mt-auto border-s-transparent border-bs-transparent border-e-(--pink) border-be-transparent border-s-0 border-bs-40 border-e-40 border-be-0"></div>
+        </div>
+      </li>
+    </ul>
+  );
 };
 
 export default Artists;
