fe86c14cd243d13219cbc40c64f62de70601a490
Ida <idla0004@stud.kea.dk>
2026-05-14 13:32:31 +0200

    Rettet importering på front page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,6 @@ import GallerySection from "./components/homepage/night_club_gallery/GallerySect
 import FeaturedSection from "./components/homepage/featuredevents_section/FeaturedSection";
 import TestimonialsSection from "./components/homepage/testimonials_section/TestimonialsSection";
 import LatestVideo from "./components/latest_video/LatestVideoSection";
-import TrackPlayer from "./components/homepage/club_track_section/TrackPlayer";
-
 export default function Home() {
   return (
     <main className="">
@@ -18,7 +16,6 @@ export default function Home() {
       <GallerySection />
       <LatestVideo />
       <TestimonialsSection />
-      <TrackPlayer />
     </main>
   );
 }
