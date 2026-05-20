047594804efdd9bac3ca970ddaf1b7e875a10f8a
Ida <idla0004@stud.kea.dk>
2026-05-20 21:45:39 +0200

    - Featured events fetched ud fra events API med isFeatured: true - Featured events gjort responsiv med klikbare knapper

--- src/app/components/homepage/featuredevents_section/FeaturedSection.jsx
+++ src/app/components/homepage/featuredevents_section/FeaturedSection.jsx
@@ -1,13 +1,15 @@
 import HomeHeadlines from "../HomeHeadlines";
 import { FeaturedSectionFetch } from "./FeaturedSectionFetch";
-
+import { Suspense } from "react";
 const FeaturedSection = ({ id }) => {
   return (
-    <section className="featuredsection relative">
+    <section className="featuredsection relative mt-medium pb-medium">
       <HomeHeadlines text="featured events" />
-      <ul className="full-width-scroller">
-        <FeaturedSectionFetch id={id} />
-      </ul>
+      <Suspense>
+        <ul className="full-width-scroller justify-center list-none p-0 w-full">
+          <FeaturedSectionFetch id={id} />
+        </ul>
+      </Suspense>
     </section>
   );
 };
